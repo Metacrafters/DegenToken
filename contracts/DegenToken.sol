73b9652ad5becbf259a5c8f71f6a290831390885
Christopher Gold <crgold@asu.edu>
2023-05-17 10:20:03 +0800

    Update DegenToken.sol
    Remove token name and minting function so student completes this requirement on their own

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    constructor() ERC20("Degen", "DGN") {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    constructor() ERC20("", "") {
+        
     }
 }
