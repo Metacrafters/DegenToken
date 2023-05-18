49e1430708a0868316d794d2cafb43c823eaaa29
Christopher Gold <crgold@asu.edu>
2023-05-18 12:00:50 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,7 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    constructor() ERC20("", "") {
-        
+
+    constructor() ERC20("Degen", "DGN") {}
+
+        function mint(address to, uint256 amount) public onlyOwner {
+            _mint(to, amount);
     }
 }
